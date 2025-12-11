b329221091632f155069866e375c2932c98546a9
ArthurFernandes8 <arthur.fernandes.falcao.araujo@ccc.ufcg.edu.br>
2025-12-11 12:41:24 -0300

    chore: configura conexão com PostgreSQL no application.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "4.0.0"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("org.hibernate.orm") version "7.1.8.Final"
+	//id("org.hibernate.orm") version "7.1.8.Final"
 	id("org.graalvm.buildtools.native") version "0.11.3"
 }
 
@@ -30,15 +30,17 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	//implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-webm")
+	implementation("com.auth0:java-jwt:4.4.0")
+
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("com.h2database:h2")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
